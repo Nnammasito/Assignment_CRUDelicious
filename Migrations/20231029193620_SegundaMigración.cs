8a1d7dbafeac23338cb3e5739c4212e4a2fd6df5
Nnammasito <nicolasgonzalezhuemur@gmail.com>
2023-10-29 16:36:33 -0300

    Migraciones

---
+++ Migrations/20231029193620_SegundaMigración.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Assignment_CRUDelicious.Migrations
+{
+    public partial class SegundaMigración : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
